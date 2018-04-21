7a6de25fdb83040e0b5abded5432865231569b48
mooracle <yanuar.prakosa@gmail.com>
2018-04-22 05:38:08 +0700

    ENTRY 10: Add new materialize designed styling for web page and create POJO model for Gif

---
+++ src/main/java/com/mooracle/giflibrary/model/Gif.java
@@ -0,0 +1,19 @@
+package com.mooracle.giflibrary.model;
+
+public class Gif {
+    /** ENTRY 10: Modelling Data with POJOs
+     *  1.  One of the most powerful feature in Spring is the ability to use Plain Old Java Object to control the
+     *      application. Since POJO is coded for its natural functionality not for the specific framework in which it
+     *      will be used, the content of POJO class will be just like any other common Java class with constructor,
+     *      getter, and setter, etc.
+     *  2.  In this ENTRY 10 we will use POJO to model (to define or to describe) how a Gif as a data should be like
+     *      and should behave. Just like any other POJO, Gif.java with have all the attributes inside parameters and
+     *      fields and also behaviors in its methods. This Gif.java POJO will be attached to each gif images
+     *      uploaded inside the gifs directory
+     *      NOTE: since it is new schema uploaded the coding and naming mught slightly different
+     *  3.  Inside class Gif put all necessary attributes into declared fields and constructor
+     *  4.  Create getter and Setter for all variables (fields)
+     *
+     *  TODO: NEXT: ENTRY 11: Feeding Data to Our Thymeleaf Templates
+     *  GOTO: GifController.java for ENTRY 11!*/
+}
